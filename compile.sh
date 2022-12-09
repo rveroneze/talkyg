3f65f5faa011794ccacc12a2899716ed2604cf6b
Rosana Veroneze <rveroneze@gmail.com>
2022-12-09 11:47:25 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -std=gnu++11 -g -Wall stdafx.cpp globalsv.cpp Utils.cpp DataUtils.cpp talkyg.cpp talkygMain.cpp -o TalkyG_mminsup
+g++ -std=gnu++11 -g -Wall -Wextra stdafx.cpp globalsv.cpp Utils.cpp DataUtils.cpp talkyg.cpp talkygMain.cpp -o TalkyG_mminsup
