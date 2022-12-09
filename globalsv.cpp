10b526ce143af0e25fda28125e611fa00e278730
Rosana Veroneze <rveroneze@gmail.com>
2022-12-09 11:49:08 +0100

    Update globalsv.cpp
    default output to python format

--- globalsv.cpp
+++ globalsv.cpp
@@ -2,7 +2,7 @@
 #include "globalsv.h"
 
 data_t MVS = 999999; // value that represents a Missing Value
-unsigned short g_output = 1; // format of the output file: 1 - matlab; 2 - python
+unsigned short g_output = 2; // format of the output file: 1 - matlab; 2 - python
 
 unsigned g_cont = 0; // number of patterns in the output
 ofstream g_filebics; // pointer to the output file
